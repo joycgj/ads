aa0fc38e38caa4dec5e3bfcb67c8d78eab3f2211
chengaojie <cgj@adeMacBook-Pro.local>
2019-04-05 22:05:22 +0800

    init

---
+++ src/main/java/com/interview/LongestCommonSubsequence.java
@@ -0,0 +1,26 @@
+package com.interview;
+
+public class LongestCommonSubsequence {
+    public int lcsDynamic(char str1[], char str2[]) {
+        int[][] dp = new int[str1.length + 1][str2.length + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+        return dp[str1.length][str2.length];
+    }
+
+    public static void main(String args[]) {
+        LongestCommonSubsequence lcs = new LongestCommonSubsequence();
+        String str1 = "ABCDGHLQR";
+        String str2 = "AEDPHR";
+
+        int result = lcs.lcsDynamic(str1.toCharArray(), str2.toCharArray());
+        System.out.print(result);
+    }
+}
